66b70a57d7350570e4460c49728c1a5833563015
Roger Sebastien <rsebasti@c2r6p8.42perpignan.fr>
2024-11-11 11:32:09 +0100

    Update

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rsebasti <rsebasti@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:01:58 by rsebasti          #+#    #+#             */
-/*   Updated: 2024/11/11 07:48:01 by rsebasti         ###   ########.fr       */
+/*   Updated: 2024/11/11 11:24:55 by rsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,45 @@ static int	putstrs(char *s)
 	return (i);
 }
 
+static int	putnbrs(int n)
+{
+	long	nb;
+	int		i;
+
+	nb = n;
+	i = 0;
+	if (nb < 0)
+	{
+		i += putchars('-');
+		nb = -nb;	
+	}
+	if (nb > 9)
+		i += putnbrs(nb / 10);
+	i += putchars(nb % 10 + 48);
+	return (i);
+}
+
+static int	putunbrs(unsigned int n)
+{
+	int		i;
+
+	i = 0;
+	if (n > 9)
+		i += putnbrs(n / 10);
+	i += putchars(n % 10 + 48);
+	return (i);
+}
+
 static int	putvar(char c, va_list arg)
 {
 	if (c == 'c')
-		return (putchar((char) arg));
+		return (putchar(va_arg(arg, char)));
 	if (c == 's')
-		return (putstrs((char *) arg));
+		return (putstrs(va_arg(arg, char *)));
 	if (c == 'p')
-		//void
-	if (c == 'd')
-		// base decimal 10
-	if (c == 'i')
-		// integer
+		return (puthexa(va_arg(arg, void *), 0));
+	if (c == 'd' || 'i')
+		return putnbrs(va_arg(arg, int));
 	if (c == 'u')
 		//unsigned decimal
 	if (c == 'x')
@@ -50,7 +77,25 @@ static int	putvar(char c, va_list arg)
 	if (c == 'X')
 		//exa maj
 	if (c == '%')
-		// %
+		return (putchar('%'));
+}
+
+static int	puthexa(long nb, int ismaj)
+{
+	char	*hexa;
+	int		i;
+
+	i = 0;
+	hexa = "0123456789abcdef";
+	if (nb < 0)
+	{
+		i += putchars('-');
+		nb = -nb;	
+	}
+	if (nb > 9)
+		i += putnbrs(nb / 10);
+	i += putchars(nb % 10 + 48);
+	return (i);
 }
 
 int	ft_printf(const char *str, ...)
@@ -75,3 +120,4 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (psize);
 }
+int
